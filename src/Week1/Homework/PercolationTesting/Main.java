c66125c1f6930b85d5f2aef0d1f279d4910b20fb
Eugene Belkovich <ebelkovich@klika-tech.com>
2017-11-13 01:31:35 +0300

    implementation of Percolation

--- src/Week1/Homework/PercolationTesting/Main.java
+++ src/Week1/Homework/PercolationTesting/Main.java
@@ -2,8 +2,11 @@ package Week1.Homework.PercolationTesting;
 
 public class Main {
     public static void main(String[] args) {
-        Percolation perc = new Percolation(10);
-        perc.print();
-        System.out.println(perc.isOpen(1,1));
+//        Percolation perc = new Percolation(5);
+//        perc.print();
+//        System.out.println(perc.isOpen(1,1));
+//        perc.open(3,3);
+//        perc.open(2,2);
+//        perc.print();
     }
 }
--- src/Week1/Homework/PercolationTesting/Percolation.java
+++ src/Week1/Homework/PercolationTesting/Percolation.java
@@ -4,90 +4,90 @@ import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 
 public class Percolation {
+    private boolean[][] opened;
+    private int top;
+    private int bottom;
+    private WeightedQuickUnionUF uf;
+    private WeightedQuickUnionUF uf2;
+    private int N;
+    private int countOpen;
 
-    int N;
-    int top;
-    boolean [][] opened;
-    WeightedQuickUnionUF uf;
-    WeightedQuickUnionUF uf2;
-
-    public Percolation(int n) throws IllegalArgumentException {
+    public Percolation(int n){
         if (n <= 0) {
-            throw new IllegalArgumentException("Check constructor parameter");
+            throw new IllegalArgumentException("Given n <= 0");
         }
-        this.N = n;
-        this.top = n*n;
-        this.opened = new boolean[n][n];
-        uf = new WeightedQuickUnionUF(n*n+2);
-    }
-
-    public void print() {
-        for (int i = 0; i < opened.length; i++) {
-            for (int j = 0; j < opened[i].length; j++) {
-                System.out.print(opened[i][j] + " ");
-            }
-            System.out.println();
-        }
-    }
-
-    public boolean isFull(int row, int col) {
-        validate(row, col);
-
-        return false;
-    }
-
-    public boolean isOpen(int row, int col) {
-        validate(row, col);
-        return this.opened[row-1][col-1];
+        opened = new boolean[n][n];
+        uf = new WeightedQuickUnionUF(n*n + 2);
+        uf2 = new WeightedQuickUnionUF(n*n + 1);
+        top = n*n;
+        bottom = n*n + 1;
+        N = n;
+        countOpen=0;
     }
 
     public void open(int row, int col) {
         validate(row, col);
         if (!isOpen(row, col)) {
-            this.opened[row-1][col-1] = true;
+            opened[row-1][col-1] = true;
+            countOpen++;
         }
         if (row == 1) {
             uf.union(col-1, top);
+            uf2.union(col-1, top);
         }
-        if (row > 1) {
-            uf.union(getCurrentSite(row, col), getLeftAdjoinSite(row, col));
+        if (row == N) uf.union(getCurrentSite(row, col), bottom);
+        if (row > 1   && isOpen(row-1, col)) {
+            uf.union(getCurrentSite(row, col), getRightSite(row, col));
+            uf2.union(getCurrentSite(row, col), getRightSite(row, col));
         }
-        if (row < N) {
-            uf.union(getCurrentSite(row, col), getRightAdjoinSite(row, col));
+        if (row < N && isOpen(row+1, col)) {
+            uf.union(getCurrentSite(row, col), getLeftSite(row, col));
+            uf2.union(getCurrentSite(row, col), getLeftSite(row, col));
         }
-        if (row == N) {
-            //todo connect with bottom
-//            uf.union(getCurrentSite(row, col), getLeftAdjoinSite(row, col));
+        if (col > 1   && isOpen(row, col-1)) {
+            uf.union(getCurrentSite(row, col), getTopSite(row, col));
+            uf2.union(getCurrentSite(row, col), getTopSite(row, col));
         }
-        if (col > 1) {
-            uf.union(getCurrentSite(row, col), getTopAdjoinSite(row, col));
-        }
-        if (col < N) {
-            uf.union(getCurrentSite(row, col), getBottomAdjoinSite(row, col));
+        if (col < N && isOpen(row, col+1)) {
+            uf.union(getCurrentSite(row, col), getBottomSite(row, col));
+            uf2.union(getCurrentSite(row, col), getBottomSite(row, col));
         }
     }
 
-    public boolean percolates() {
-        return false;
+    private int getCurrentSite(int row,int col){
+        return (row-1)* N +col-1;
     }
 
-    private int getCurrentSite(int row, int col){
-        // get position in flat matrix
-        return (row-1)*N+(col-1);
-    }
-    private int getLeftAdjoinSite(int row, int col){
-        return (row-2)*N+(col-1);
-    }
-    private int getRightAdjoinSite(int row, int col){
-        return (row)*N+(col-1);
-    }
-    private int getTopAdjoinSite(int row, int col){
-        return (row)*N+(col-2);
-    }
-    private int getBottomAdjoinSite(int row, int col){
-        return (row)*N+(col);
+    private int getLeftSite(int row,int col){
+        return row* N +col-1;
     }
 
+    private int getTopSite(int row,int col){
+        return (row-1)* N +col-2;
+    }
+
+    private int getBottomSite(int row,int col){
+        return (row-1)* N +col;
+    }
+
+    private int getRightSite(int row,int col){
+        return (row-2)* N +col-1;
+    }
+
+    public boolean isOpen(int i, int j){
+        validate(i, j);
+        return opened[i-1][j-1];
+    }
+
+    public boolean isFull(int i, int j){
+        validate(i, j);
+        return uf2.connected((i-1)* N +j-1, top);
+    }
+
+    public boolean percolates()
+    {
+        return uf.connected(top, bottom);
+    }
 
     private void validate(int row, int col) {
         if (row <= 0 || row > N ||  col <= 0 || col > N) {
@@ -95,6 +95,7 @@ public class Percolation {
         }
     }
 
-
+    public int numberOfOpenSites(){
+        return countOpen;
+    }
 }
-
